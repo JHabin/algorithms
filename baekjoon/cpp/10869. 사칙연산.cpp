c236615110947769b3e851350f82729666f7a845
JHabin <habin424@gmail.com>
2024-10-15 12:57:14 +0900

    Create 10869. 사칙연산.cpp

---
+++ baekjoon/cpp/10869. 사칙연산.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+  
+    int n1, n2;
+    cin >> n1 >> n2;
+    cout << n1 + n2 << endl << n1 - n2 << endl << n1 * n2 << endl << n1 / n2 << endl << n1%n2;
+    return 0;
+}
