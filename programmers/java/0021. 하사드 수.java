b72bcf674c50d361982aff0ce668e0548135989d
JHabin <habin424@gmail.com>
2024-10-23 20:29:26 +0900

    Create 0021. 하사드 수.java

---
+++ programmers/java/0021. 하사드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int tmp = x;
+        while(tmp != 0){
+            sum += tmp % 10;
+            tmp /= 10;
+        }
+        answer = (x%sum == 0)? true : false;
+        return answer;
+    }
+}
