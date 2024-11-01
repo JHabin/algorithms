14d2ca40c1c0b06b70e649737a54703ca6fb3cd1
JHabin <habin424@gmail.com>
2024-11-01 12:04:23 +0900

    Create 27866. 문자와 문자열.cpp

---
+++ baekjoon/cpp/27866. 문자와 문자열.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string inputS;
+    int index;
+    cin >> inputS;
+    cin >> index;
+    cout << inputS[index-1];
+}
