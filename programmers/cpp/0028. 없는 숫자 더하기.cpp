2c475b78f1adaf95fc6f80d58edc16c0910f488a
JHabin <habin424@gmail.com>
2024-11-03 23:45:48 +0900

    Update 0028. 없는 숫자 더하기.cpp

--- programmers/cpp/0028. 없는 숫자 더하기.cpp
+++ programmers/cpp/0028. 없는 숫자 더하기.cpp
@@ -1,12 +1,11 @@
-#include <string>
+#include <numeric>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<int> numbers) {
     int answer = 45;
-    for (int i = 0; i < numbers.size(); i++){
-        answer -= numbers[i];
-    }
+    answer -= accumulate(numbers.begin(), numbers.end(), 0);
+    
     return answer;
 }
