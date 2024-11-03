f6641a9eaabe197b9338ee8a9f4576bc074c948a
JHabin <habin424@gmail.com>
2024-11-04 02:21:11 +0900

    Create 0029. 제일 작은 수 제거하기.cpp

---
+++ programmers/cpp/0029. 제일 작은 수 제거하기.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    int min = *min_element(arr.begin(), arr.end());
+    for (int a : arr){
+        if (a != min) answer.push_back(a);
+    }
+    if (answer.size() == 0) answer.push_back(-1);
+    return answer;
+}
