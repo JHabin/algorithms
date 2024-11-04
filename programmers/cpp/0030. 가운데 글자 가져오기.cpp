bd367f04d63cf8eae4a0cc24571aa42f1d8a416d
JHabin <habin424@gmail.com>
2024-11-04 23:25:45 +0900

    Create 0030. 가운데 글자 가져오기.cpp

---
+++ programmers/cpp/0030. 가운데 글자 가져오기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int size = s.size();
+    if (size % 2 == 1) answer = s.substr(size/2, 1);
+    else answer = s.substr((size/2) - 1,2);
+    return answer;
+}
